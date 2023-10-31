f71589114c1e398f0a85097cc5a54b7d24d6c2d0
timotay <timotay@pop-os.localdomain>
2023-10-31 06:53:42 +0000

    added blob

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# TTTS
+
+## Intro
+
+
+## Setup
+### Virtual Environments
+``pip install virtualenv``
+``cd /path/to/project``
+``virtualenv TTTS``
+``source TTTS/bin/activate``
+``pip install -r requirements.txt``
+
+
+### Azure
+Set up the following resources and create a file called ``config.py``. Use the example config in the repo to fill out your  secrets
--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 import uuid
 from flask import Flask, render_template, request, redirect, url_for
-#from azure.storage.blob import BlobServiceClient
-#from azure.cognitiveservices.speech import SpeechConfig, AudioConfig, SpeechSynthesizer
+from azure.storage.blob import BlobServiceClient
 import config
 app = Flask(__name__)
 import ttts_translate as translate
+import azure.cognitiveservices.speech as speechsdk
+from azure.storage.blob import BlobServiceClient
+
+
 
 # Azure Cognitive Services configuration
 azure_cognitive_endpoint = config.azureend
@@ -13,8 +16,6 @@ azure_cognitive_key = config.key1
 
 print(azure_cognitive_endpoint)
 
-import os
-import azure.cognitiveservices.speech as speechsdk
 
 # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
 speech_config = speechsdk.SpeechConfig(subscription= azure_cognitive_key, region='westeurope')
@@ -29,10 +30,54 @@ speech_config.speech_synthesis_voice_name='ca-ES-EnricNeural'
 
 speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
+# blob config
+azure_storage_connection_string = config.blobstring
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    user_text = ''
+    if request.method == 'POST':
+        text = request.form['text']
+        user_text = translate.translate(text)
 
+        # Save user_text to Azure Blob Storage
+        save_user_text_to_blob(user_text)
 
+    # Retrieve and display the last 10 saved items
+    saved_items = get_last_10_saved_items()
+    return render_template('index.html', user_text=user_text, saved_items=saved_items)
+
+def save_user_text_to_blob(user_text):
+    blob_service_client = BlobServiceClient.from_connection_string(azure_storage_connection_string)
+    container_name = "user-texts"  # Replace with your desired container name
+    container_client = blob_service_client.get_container_client(container_name)
+    blob_name = str(uuid.uuid4()) + ".txt"  # Generate a unique blob name
+
+    blob_client = container_client.get_blob_client(blob_name)
+    blob_client.upload_blob(user_text, overwrite=True)
+
+def get_last_10_saved_items():
+    blob_service_client = BlobServiceClient.from_connection_string(azure_storage_connection_string)
+    container_name = "user-texts"  # Replace with your container name
+    container_client = blob_service_client.get_container_client(container_name)
+
+    # List blobs and sort them by creation time in descending order
+    blobs = sorted(container_client.list_blobs(), key=lambda x: x.creation_time, reverse=True)
+
+    saved_items = []
+    for blob in blobs[:10]:  # Retrieve the last 10 blobs
+        blob_client = container_client.get_blob_client(blob.name)
+        blob_content = blob_client.download_blob()
+        saved_text = blob_content.content_as_text()
+        saved_items.append(saved_text)
+
+    return saved_items
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+'''
 @app.route('/', methods=['GET', 'POST'])
 def index():
     user_text = ''
@@ -44,3 +89,4 @@ def index():
 if __name__ == '__main__':
     app.run(debug=True)
 
+'''
